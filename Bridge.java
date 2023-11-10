c0f0e04a86b23fe8cd6e5e2003d44cf85e5aa213
Srivennala-Kandibedala <srivennala.gma@gmail.com>
2023-11-09 22:25:15 -0500

    second change

--- Bridge.java
+++ Bridge.java
@@ -1,6 +1,20 @@
-/**
- * Bridge
- */
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 public class Bridge {
     String lan_name;
     String num_ports;
@@ -8,8 +22,119 @@ public class Bridge {
         lan_name = l_name;
         num_ports = n_ports;
     }
+    private static final Signal SIGNAL = new Signal("INT");
+    public static List<SocketChannel> activeClients = new ArrayList<>();
+    private static ServerSocketChannel serverChannel;
+    private static Selector selector;
+    private static boolean serverRunning = true;
+    private static final SignalHandler signalHandler = signal -> {
+        try {
+            serverRunning = false;
+            selector.keys().forEach(SelectionKey::cancel);
+            selector.close();
+            serverChannel.close();
+            System.out.println("Server Exiting...");
+            System.exit(0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    };
+    private static void createAddressSymbolicLink(String lanName, String ipAddress) {
+        try {
+            Path linkPath = Paths.get("." + lanName + ".addr");
+            Path targetPath = Paths.get(ipAddress);
+            Files.createSymbolicLink(linkPath, targetPath);
+        } catch (IOException e) {
+            throw new RuntimeException("Error creating address symbolic link: " + e.getMessage());
+        }
+    }
+
+    private static void createPortSymbolicLink(String lanName, int portNumber) {
+        try {
+            Path linkPath = Paths.get("." + lanName + ".port");
+            Path targetPath = Paths.get(String.valueOf(portNumber));
+            Files.createSymbolicLink(linkPath, targetPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error creating port symbolic link: " + e.getMessage());
+        }
+    }
     public static void main(String args[]){
         Bridge b1 = new Bridge(args[0],args[1]);
         System.out.println(b1.lan_name + ',' + b1.num_ports);
+        try {
+            serverChannel = ServerSocketChannel.open();
+            selector = Selector.open();
+            serverChannel.configureBlocking(false);
+            InetSocketAddress address = new InetSocketAddress(InetAddress.getLocalHost(), 0);
+            serverChannel.bind(address);
+
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            Signal.handle(SIGNAL, signalHandler);
+
+            String ipAddress = serverChannel.socket().getInetAddress().getHostName();
+            createAddressSymbolicLink(b1.lan_name, ipAddress);
+    
+            int portNumber = serverChannel.socket().getLocalPort();
+            createPortSymbolicLink(b1.lan_name, portNumber);
+
+            while (serverRunning) {
+                selector.select();
+
+                if (serverRunning) {
+                    selector.selectedKeys().forEach((socket) -> {
+                        if (socket.isAcceptable()) {
+                            try {
+                                ServerSocketChannel server = (ServerSocketChannel) socket.channel();
+                                SocketChannel client = server.accept();
+                                client.configureBlocking(false);
+                                client.register(selector, SelectionKey.OP_READ);
+                                activeClients.add(client);
+                                System.out.println("Connected from: " + client.getRemoteAddress().toString().split("/")[1]);
+                            } catch (IOException e) {
+                                System.out.println("Cannot accept connection");
+                            }
+                        } else if (socket.isReadable()) {
+                            handleClient(socket);
+                        }
+                    });
+                    selector.selectedKeys().clear();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void handleClient(SelectionKey socket) {
+        try {
+            SocketChannel client = (SocketChannel) socket.channel();
+            ByteBuffer bytes = ByteBuffer.allocate(1024);
+
+            int read = client.read(bytes);
+            if (read < 0) {
+                System.out.println("Disconnected from: " + client.getRemoteAddress().toString().split("/")[1]);
+                activeClients.remove(client);
+                client.close();
+                socket.cancel();
+                return;
+            }
+
+            bytes.flip();
+            String str = StandardCharsets.UTF_8.decode(bytes).toString();
+            bytes.clear();
+
+            for (SocketChannel otherClient : activeClients) {
+                if (client != otherClient) {
+                    ByteBuffer byteBuffer = ByteBuffer.wrap(str.getBytes());
+                    otherClient.write(byteBuffer);
+                } else {
+                    String messageStr = otherClient.getRemoteAddress().toString().split("/")[1] + " -> " + str;
+                    System.out.println(messageStr);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- Station.java
+++ Station.java
@@ -1,7 +1,24 @@
 import java.io.*;
+import java.nio.channels.SocketChannel;
 import java.util.*;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 public class Station {
     String comp_name, iface, rtable, htable;
+    private static final Signal SIGNAL = new Signal("INT");
+    private static final SignalHandler signalHandler = signal -> {
+        quit();
+    };
+    private static SocketChannel socket;
+    private static Thread receivingThread;
 
     public Station(String name, String i_face, String r_table, String h_table) {
         comp_name = name;
@@ -12,11 +29,11 @@ public class Station {
 
     public static void main(String[] args) {
         Station s1 = new Station(args[0], args[1], args[2], args[3]);
-        // System.out.println(s1.comp_name+","+ s1.iface+","+ s1.rtable+","+s1.htable);
         System.out.println();
         System.out.println("initializing..");
         System.out.println();
         //Loading and Printing ifaces
+        Vector<Vector<String>> ifaceData = new Vector<>();
         try{
             File ifacePath = new File("ifaces");
             File ifaceList[] = ifacePath.listFiles();
@@ -30,7 +47,7 @@ public class Station {
             System.out.println("reading ifaces... ");
             System.out.println();
             Scanner myReader = new Scanner(matchingFile);
-            Vector<Vector<String>> ifaceData = new Vector<>();
+            // Vector<Vector<String>> ifaceData = new Vector<>();
             while (myReader.hasNextLine()) {
                 String[] ifaceLine = myReader.nextLine().trim().split("\\s+");
                 Vector<String> line = new Vector<>();
@@ -106,5 +123,64 @@ public class Station {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        try {
+            if (args.length < 2) {
+                System.out.println("Usage: java chatclient <host name> <port>");
+                return;
+            }
+            String ipAddress = "", portNumber = "";
+            for (Vector<String> line : ifaceData){
+                Path addressLink = Paths.get("." + line.lastElement()+ ".addr");
+                ipAddress = Files.readSymbolicLink(addressLink).toString();
+
+                Path portLink = Paths.get("." + line.lastElement() + ".port");
+                portNumber = Files.readSymbolicLink(portLink).toString();
+            }
+            socket = SocketChannel.open(new InetSocketAddress(ipAddress, Integer.parseInt(portNumber)));
+            Scanner scanner = new Scanner(System.in);
+
+            Signal.handle(SIGNAL, signalHandler);
+
+            System.out.println("Connected to the Chat Server at: " + socket.socket().getRemoteSocketAddress() + " through port " + socket.socket().getLocalPort());
+
+            receivingThread = new Thread(() -> {
+                try {
+                    ByteBuffer bytes = ByteBuffer.allocate(1024);
+                    while (socket.read(bytes) > 0) {
+                        bytes.flip();
+                        System.out.print("Received -> ");
+                        System.out.println(StandardCharsets.UTF_8.decode(bytes));
+                        bytes.clear();
+                    }
+
+                    System.out.println("Server Closed!");
+                    quit();
+                } catch (IOException ignore) {
+
+                }
+            });
+            receivingThread.start();
+
+            System.out.println("You can start chatting now. Press CTRL + C to quit.");
+            String userInput;
+            while ((userInput = scanner.nextLine()) != null) {
+                ByteBuffer byteBuffer = ByteBuffer.wrap(userInput.getBytes());
+                socket.write(byteBuffer);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Unable to create socket!");
+        }
+    }
+
+    public static void quit() {
+        try {
+            socket.close();
+            receivingThread.interrupt();
+        } catch (IOException e) {
+            System.out.println("Cannot close socket!");
+        }
+        System.out.println("Exiting ...");
+        System.exit(0);
     }
 }
