010a438c705d3cda16427b0348cd2bb541283405
har-shaw <8888harsha4444@gmail.com>
2023-11-27 20:22:38 -0500

    Correct script file

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,20 +1,29 @@
-#!/bin/bash
+#!/bin/csh -f
 
-rm -f .cs*
+cd $PWD
 
-xterm -T "BRIDGE cs1" -iconic -e java Bridge cs1 1 &
-xterm -T "BRIDGE cs2" -iconic -e java Bridge cs2 8 &
-xterm -T "BRIDGE cs3" -iconic -e java Bridge cs3 8 &
+rm .cs*
 
-xterm -T "Host A" -e java Station -no ifaces/ifaces.a rtables/rtable.a hosts &
+make clean
+sleep 2
+make
 
-xterm -T "host B" -e java Station -no ifaces/ifaces.b rtables/rtable.b hosts &
+osascript -e 'tell application "Terminal" to do script "cd /Users/srivennalakandibedala/Datacom/Project2 && java Bridge cs1 8"'
+sleep 2
+osascript -e 'tell application "Terminal" to do script "cd /Users/srivennalakandibedala/Datacom/Project2 && java Bridge cs2 8"'
+#sleep 2
+osascript -e 'tell application "Terminal" to do script "cd /Users/srivennalakandibedala/Datacom/Project2 && java Bridge cs3 8"'
 
-xterm -T "host C" -e java Station -no ifaces/ifaces.c rtables/rtable.c hosts &
-
-xterm -T "host D" -e java Station -no ifaces/ifaces.d rtables/rtable.d hosts &
-
-xterm -T "host E" -e java Station -no ifaces/ifaces.e rtables/rtable.e hosts &
-
-xterm -T "Router r1" -iconic -e java Station -route ifaces/ifaces.r1 rtables/rtable.r1 hosts &
-xterm -T "Router r2" -iconic -e java Station -route ifaces/ifaces.r2 rtables/rtable.r2 hosts &
+sleep 2
+osascript -e 'tell application "Terminal" to do script "cd /Users/srivennalakandibedala/Datacom/Project2 && java Station -no ifaces/ifaces.a rtables/rtable.a hosts"'
+sleep 1
+osascript -e 'tell application "Terminal" to do script "cd /Users/srivennalakandibedala/Datacom/Project2 && java Station -no ifaces/ifaces.b rtables/rtable.b hosts"'
+sleep 1
+osascript -e 'tell application "Terminal" to do script "cd /Users/srivennalakandibedala/Datacom/Project2 && java Station -no ifaces/ifaces.c rtables/rtable.c hosts"'
+sleep 1
+osascript -e 'tell application "Terminal" to do script "cd /Users/srivennalakandibedala/Datacom/Project2 && java Station -no ifaces/ifaces.d rtables/rtable.d hosts"'
+sleep 1
+osascript -e 'tell application "Terminal" to do script "cd /Users/srivennalakandibedala/Datacom/Project2 && java Station -no ifaces/ifaces.e rtables/rtable.e hosts"'
+sleep 2
+osascript -e 'tell application "Terminal" to do script "cd /Users/srivennalakandibedala/Datacom/Project2 && java Station -route ifaces/ifaces.r1 rtables/rtable.r1 hosts"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/srivennalakandibedala/Datacom/Project2 && java Station -route ifaces/ifaces.r2 rtables/rtable.r2 hosts"'
